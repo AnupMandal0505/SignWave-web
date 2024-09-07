74b988885f3e727dcd9310acf351d56b7e4f2a25
Anupam Kumar Sourav <anupam.sony32@gmail.com>
2024-09-07 12:06:32 +0530

    :speech:

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,120 +1,115 @@
-import { useRef, useEffect, useState } from 'react';
-import { Hands, HAND_CONNECTIONS } from '@mediapipe/hands';
-import { drawConnectors, drawLandmarks } from '@mediapipe/drawing_utils';
-import * as cam from '@mediapipe/camera_utils';
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 
-import white from '../res/white.jpg'
+const Contact = () => {
 
-function Contact() {
-  const videoRef = useRef(null);
-  const canvasRef = useRef(null);
-  const cameraRef = useRef(null);
-  const [imgSrc, setImgSrc] = useState('');
-  const [text, setText] = useState("")
-
-
-  useEffect(() => {
-    return;
-    const api_call = async () => {
-      await fetch('http://127.0.0.1:5000/pred', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ img: imgSrc })
-      }).then(res => res.json()).then(data => setText(text + data['char']))
-    }
-
-    api_call()
-  }, [imgSrc]);
-
-
-  useEffect(() => {
-    const hands = new Hands({
-      locateFile: (file) => `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`,
-    });
-
-    hands.setOptions({
-      maxNumHands: 1,
-      modelComplexity: 1,
-      minDetectionConfidence: 0.7,
-      minTrackingConfidence: 0.7,
-    });
-
-    hands.onResults((results) => {
-      const processResults = async () => {
-        const canvas = canvasRef.current;
-        const canvasCtx = canvas.getContext('2d');
-        canvasCtx.save();
-
-        // Clear the canvas to ensure transparency
-        canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
-
-        // Draw landmarks if available
-        if (results.multiHandLandmarks) {
-          for (const landmarks of results.multiHandLandmarks) {
-            drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, {
-              color: 'lightgreen',
-              lineWidth: 2,
-            });
-
-            drawLandmarks(canvasCtx, landmarks, {
-              color: 'lightgreen',
-              lineWidth: 2,
-              radius: 1,
-            });
-          }
-        }
-        canvasCtx.restore();
-        setImgSrc(canvas.toDataURL('image/png'));
-      };
-      // Set a delay before processing results
-      const timeoutId = setTimeout(processResults, 800); // 1000ms delay (1 second)
-
-      // Clear the timeout if the component unmounts or before setting a new timeout
-      return () => clearTimeout(timeoutId);
-
-    });
-
-    if (videoRef.current) {
-      cameraRef.current = new cam.Camera(videoRef.current, {
-        onFrame: async () => {
-          await hands.send({ image: videoRef.current });
-        },
-        width: 640,
-        height: 480,
-      });
-      cameraRef.current.start();
-    }
-  }, []);
 
   return (
-    <div style={{ paddingTop: '20vh' }}>
-      <div className="container">
-        <div className="h-full w-full" style={{}}>
-          <div className="w-full col-md-8 flex justify-end items-end h-[30rem] bg-cover bg-center  border-gray-200">
-            <video
-              autoPlay
-              ref={videoRef}
-              className='absolute h-full w-full z-[1] shadow border' 
-              style={{objectFit:'cover',borderRadius:'20px'}}
-            />
-            <canvas
-              ref={canvasRef}
-              className='absolute h-full w-full z-[2] bg-transparent'
-            />
+    <>
+      <Navbar />
+
+      <div className="container-fluid mt-16">
+        <div className="row justify-content-center text-center">
+          <div className="col-12 col-md-12">
+            <h1 ><span>-</span>Contact Us<span>-</span></h1>
+            <p  >
+              Explore Our Recent Creations, Demonstrating Our Commitment to Delivering Transformative Digital Solutions.
+            </p>
+            <button type="button" className="btn btn-lg btn-primary mb-3 mb-md-4 mb-xl-5">Connect Now</button>
           </div>
         </div>
       </div>
-        <p>{text}</p>
-    </div>
+      <div className="container-fluid opacity-90   " >
+        <div className="row" style={{
+          backgroundImage: `url('https://cdn.pixabay.com/photo/2015/09/21/23/28/love-sign-950912_1280.jpg')`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center',
+        }}>
+          <div className="col-md-6 flex flex-col justify-center items-center text-center text-white">
+            <h1>Contact Us</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim ratione, accusamus ad sit error cum!</p>
+          </div>
+
+          <div className="col-md-6">
+            <div className="max-w-sm mx-auto m-5">
+              <div className="mb-5">
+                <label
+                  htmlFor="email"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Your email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                  placeholder="name@flowbite.com"
+                  required
+                />
+              </div>
+
+              <div className="mb-5">
+                <label
+                  htmlFor="Full Name"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Your Full Name
+                </label>
+                <input
+                  type="text"
+                  id="First Name"
+                  className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                  required
+                />
+              </div>
+
+              <div className="mb-5">
+                <label
+                  htmlFor="Phone"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Phone number
+                </label>
+                <input
+                  type="number"
+                  id="Phone"
+                  className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                  required
+                />
+              </div>
+
+              <div className="flex items-start mb-5">
+                <div className="flex items-center h-5">
+                  <input
+                    id="terms"
+                    type="checkbox"
+                    className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+                    required
+                  />
+                </div>
+                <label
+                  htmlFor="terms"
+                  className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >
+                  I agree with the{" "}
+                  <a href="#" className="text-blue-600 hover:underline dark:text-blue-500">
+                    terms and conditions
+                  </a>
+                </label>
+              </div>
+
+              <button
+                type="submit"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Register new account
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <Footer />
+    </>
   );
-}
+};
 
 export default Contact;
-
-
-/*
-
-      {imgSrc && <img src={imgSrc} width={640} height={480} alt="Canvas content" />}
-*/
--- frontend/src/pages/VideoCall2.jsx
+++ frontend/src/pages/VideoCall2.jsx
@@ -22,7 +22,13 @@ function VideoCall2() {
     const [receiverName, setReceiverName] = useState(null);
     const [loading, setLoading] = useState(false);
     const [text, setText] = useState("")
-    // const [counts, setCounts] = useState(0);
+    const [transformType, setTransformType] = useState('SIGN');
+
+    const transformOptions = [
+        {value:'SIGN'},
+        {value:'SPEECH'},
+    ]
+
     var counts = 0;
 
     var prevChar = null;
@@ -90,90 +96,205 @@ function VideoCall2() {
         return char
     }
 
+    // SIGN
+    const hands = new Hands({
+        locateFile: (file) => `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`,
+    });
+    // setLoading(false);
+
+    hands.setOptions({
+        maxNumHands: 1,
+        modelComplexity: 1,
+        minDetectionConfidence: 0.7,
+        minTrackingConfidence: 0.7,
+    });
+
+    hands.onResults(async (results) => {
+        try {
+            console.log("results", results);
+            var resultData = null
+            if (results.multiHandLandmarks.length > 0) {
+                resultData = await getPrediction({ multiHandLandmarks: results.multiHandLandmarks });
+            }
+
+            const canvas = canvasRef.current;
+            const canvasCtx = canvas.getContext('2d');
+            canvasCtx.save();
+
+            // Clear the canvas to ensure transparency
+            canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
+
+            // Draw landmarks if available
+            if (results.multiHandLandmarks) {
+                for (const landmarks of results.multiHandLandmarks) {
+                    drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, {
+                        color: 'lightgreen',
+                        lineWidth: 2,
+                    });
+
+                    drawLandmarks(canvasCtx, landmarks, {
+                        color: 'lightgreen',
+                        lineWidth: 2,
+                        radius: 1,
+                    });
+                }
+            }
+            if (resultData) {
+                // Draw text on the canvas
+                canvasCtx.font = '20px Arial'; // Set the font size and type
+                canvasCtx.fillStyle = 'red';    // Set the text color
+                canvasCtx.textAlign = 'center'; // Set text alignment
+                canvasCtx.textBaseline = 'middle'; // Set text baseline
+
+                // Example text and position
+                const text = resultData;
+                const x = canvas.width / 2; // Centered horizontally
+                const y = canvas.height / 2; // Centered vertically
+
+                // Draw the text
+                canvasCtx.fillText(text, x, y);
+            }
+            canvasCtx.restore();
+        }
+        catch (err) {
+            console.log("hands.onResults Error:", err)
+        }
+
+    });
+    // SIGN
+
+
+    // SPEECH
+    // const [isRecording, setIsRecording] = useState(false);
+    // const [intervalId, setIntervalId] = useState(null);
+    const captureAudio = async (videoRef) => {
+        console.log("videoRef.current.readyState", videoRef.current.readyState);
+        // return;
+        if(videoRef.current.readyState !== 4){
+            console.log("videoRef.readyState !== 4");
+            return;
+        }
+        // Get the audio track from the stream
+        const stream = videoRef.current.srcObject;
+        const audioTrack = stream.getAudioTracks()[0];
+    
+        if (!audioTrack) {
+            console.error('No audio track found.');
+            return;
+        }
+    
+        const audioStream = new MediaStream([audioTrack]);
+    
+        try {
+            // Try different MIME types if needed
+            const mimeTypes = ['audio/webm', 'audio/webm;codecs=opus', 'audio/ogg'];
+            let selectedMimeType = null;
+    
+            for (const mimeType of mimeTypes) {
+                console.log(mimeType, MediaRecorder.isTypeSupported(mimeType))
+                if (MediaRecorder.isTypeSupported(mimeType)) {
+                    selectedMimeType = mimeType;
+                    break;
+                }
+            }
+    
+            if (!selectedMimeType) {
+                throw new Error('No supported MIME type found for MediaRecorder.');
+            }
+    
+            console.log('Using MIME type:', selectedMimeType);
+    
+            const mediaRecorder = new MediaRecorder(audioStream, { mimeType: selectedMimeType });
+            const audioChunks = [];
+    
+            mediaRecorder.ondataavailable = event => {
+                if (event.data.size > 0) {
+                    audioChunks.push(event.data);
+                }
+            };
+    
+            mediaRecorder.onstop = async () => {
+                const audioBlob = new Blob(audioChunks, { type: selectedMimeType });
+                const formData = new FormData();
+                // formData.append('audio_file', audioBlob, 'audio.' + selectedMimeType.split('/')[1]);
+                formData.append('audio_file', audioBlob, 'audio.' + "webm");
+    
+                try {
+                    const response = await fetch(DJANGO_URL+'/api/audio', {
+                        method: 'POST',
+                        body: formData
+                    });
+    
+                    if (response.ok) {
+                        const result = await response.json();
+                        setText((prev)=>prev+String(result.transcribed_text));
+                        console.log('Transcribed text:', result.transcribed_text);
+                    } else {
+                        console.error('Failed to transcribe audio');
+                    }
+                } catch (error) {
+                    console.error('Error:', error);
+                }
+            };
+    
+            mediaRecorder.onerror = (event) => {
+                console.error('MediaRecorder error:', event.error);
+            };
+    
+            mediaRecorder.start();
+            console.log('MediaRecorder started');
+    
+            // Stop recording after 5 seconds
+            setTimeout(() => {
+                mediaRecorder.stop();
+                console.log('MediaRecorder stopped');
+            }, 10000);
+    
+        } catch (error) {
+            console.error('Error initializing MediaRecorder:', error);
+        }
+    };
+    
+    // SPEECH
+
 
     useEffect(() => {
         fetchUsers();
         // setLoading(true);
-        const hands = new Hands({
-            locateFile: (file) => `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`,
-        });
-        // setLoading(false);
-
-        hands.setOptions({
-            maxNumHands: 1,
-            modelComplexity: 1,
-            minDetectionConfidence: 0.7,
-            minTrackingConfidence: 0.7,
-        });
-
-        hands.onResults(async (results) => {
-            try {
-                console.log("results", results);
-                var resultData = null
-                if (results.multiHandLandmarks.length > 0) {
-                    resultData = await getPrediction({ multiHandLandmarks: results.multiHandLandmarks });
-                }
-
-                const canvas = canvasRef.current;
-                const canvasCtx = canvas.getContext('2d');
-                canvasCtx.save();
-
-                // Clear the canvas to ensure transparency
-                canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
-
-                // Draw landmarks if available
-                if (results.multiHandLandmarks) {
-                    for (const landmarks of results.multiHandLandmarks) {
-                        drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, {
-                            color: 'lightgreen',
-                            lineWidth: 2,
-                        });
-
-                        drawLandmarks(canvasCtx, landmarks, {
-                            color: 'lightgreen',
-                            lineWidth: 2,
-                            radius: 1,
-                        });
-                    }
-                }
-                if (resultData) {
-                    // Draw text on the canvas
-                    canvasCtx.font = '20px Arial'; // Set the font size and type
-                    canvasCtx.fillStyle = 'red';    // Set the text color
-                    canvasCtx.textAlign = 'center'; // Set text alignment
-                    canvasCtx.textBaseline = 'middle'; // Set text baseline
-
-                    // Example text and position
-                    const text = resultData;
-                    const x = canvas.width / 2; // Centered horizontally
-                    const y = canvas.height / 2; // Centered vertically
-
-                    // Draw the text
-                    canvasCtx.fillText(text, x, y);
-                }
-                canvasCtx.restore();
-            }
-            catch (err) {
-                console.log("hands.onResults Error:", err)
-            }
-
-        });
-
+        var SIGN_interval_id=null;
+        var SPEECH_interval_id=null;
         if (socketContext.remoteVideoRef.current) {
-            setInterval(() => {
-                // console.log("checking")
+            if(transformType==='SIGN')
+                SIGN_interval_id=setInterval(() => {
+            // console.log("checking")
+            if (
+                socketContext?.remoteVideoRef.current !== undefined &&
+                socketContext?.remoteVideoRef.current !== null &&
+                socketContext?.remoteVideoRef.current.readyState === 4
+                
+            ) {
+                // console.log("detecting")
+                detect(hands, socketContext?.remoteVideoRef);
+            }
+            
+        }, 500);
+        if(transformType==='SPEECH')
+            SPEECH_interval_id = setInterval(() => {
                 if (
                     socketContext?.remoteVideoRef.current !== undefined &&
                     socketContext?.remoteVideoRef.current !== null &&
                     socketContext?.remoteVideoRef.current.readyState === 4
-                ) {
-                    // console.log("detecting")
-                    detect(hands, socketContext?.remoteVideoRef);
-                }
 
-            }, 500);
+                )
+                captureAudio(socketContext.remoteVideoRef);
+            }, 10000);
         }
-    }, []);
+
+        return ()=>{
+            if(SIGN_interval_id) clearInterval(SIGN_interval_id);
+            if(SPEECH_interval_id) clearInterval(SPEECH_interval_id);
+        }
+    }, [transformType]);
 
     const detect = async (hands, videoRef) => {
         await hands.send({ image: videoRef.current });
@@ -191,14 +312,20 @@ function VideoCall2() {
                 {socketContext.remoteUserName ? <button
                     onClick={() => { location.reload() }}
                     className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600 transition duration-150"
-                >
+                    >
                     End
                 </button> : <button
                     onClick={() => socketContext?.callUser(receiverName)}
                     className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 transition duration-150"
-                >
+                    >
                     Call
                 </button>}
+                <SelectComponent
+                    selectedOption={transformType}
+                    options={transformOptions}
+                    onChange={(value) => setTransformType(value)}
+                    className="border border-gray-300 rounded-md shadow-sm focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                />
             </div>
             <div className='flex flex-col justify-center flex-wrap'>
                 <h4>Translated Text:</h4>
